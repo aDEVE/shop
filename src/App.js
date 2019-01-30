9393ef09735fde7b9777631b3b697670df90fc38
adeve <deve.alexandre@gmail.com>
2019-01-30 18:46:44 +0100

    add prettier, lint and eject webpack config

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        From scratch
-      </div>
-    );
+    return <div>From scratch</div>
   }
 }
 
-export default App;
+export default App
